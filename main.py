1d68b11639aa8cd951109d9e2d017e30391e21d8
stricklandrw <stricklandrw@gmail.com>
2025-12-26 18:21:59 -0500

    Ch2: L1 complete

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\r\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
